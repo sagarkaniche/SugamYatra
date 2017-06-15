8bd1477b4db7b3f7fb4d0b115c30d48edf3440e9
sagarkaniche <sagarkaniche007@gmail.com>
2017-06-15 15:24:17 +0530

    Yo Commit

--- app/src/main/java/com/example/shchaura/sugamyatra/Index.java
+++ app/src/main/java/com/example/shchaura/sugamyatra/Index.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class Index extends AppCompatActivity {
 
     int hot = 1;
+    int cold = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
